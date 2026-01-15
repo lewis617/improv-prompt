795bdb0956c1bcb2b9da94d070d9870f67cb64db
lewis617 <lewis617@163.com>
2026-01-15 18:12:23 +0800

    feat: Add comprehensive Tapping exercises to improvisation data

--- js/data.js
+++ js/data.js
@@ -69,6 +69,42 @@ export function createImprovisationData() {
                 createNode("Diatonic Scales"),
             ]),
         ]),
+        createNode("Tapping", [
+            createNode("Triad", [
+                createNode("Descending (T-p-p)"),
+                createNode("Eruption Style (T-p-h)"),
+                createNode("Descending & Ascending (T-p-p-h)"),
+                createNode("Sextuplet (T-p-T-p-p-h)"),
+                createNode("Triad with Open String (T-p-h-h)")
+            ]),
+            createNode("Triad & Scale", [
+                createNode("Descending (T-p-p-p)"),
+                createNode("Eruption Style (T-p-h-h)"),
+                createNode("Descending & Ascending (T-p-p-p-h-h)"),
+                createNode("Alternating (T-p-h-h & T-p-p-p)")
+            ]),
+            createNode("Tapped Slides", [
+                createNode("Two-Pitch Slide (T-sl-sl-p-p-h)"),
+                createNode("Sliding between different positions (T-p-p-p-sl-h-h)")
+            ]),
+            createNode("Tapped Bends", [
+                createNode("Tap on Bent String (b-T-p-r)"),
+                createNode("Bend Tapped Note (h-T-b)")
+            ]),
+            createNode("Pentatonic Tapping", [
+                createNode("3 Tapped Positions - 2 Strings (T-p-h)"),
+                createNode("2 Tapped Positions - 6 Strings (T-p-h)")
+            ]),
+            createNode("Pedal Point Tapping", [
+                createNode("Descending 3rds & 4ths (T-p-T-p)"),
+                createNode("Movable Nut (T-h-h-p-h-h)")
+            ]),
+            createNode("Hammer-on from Nowhere", [
+                createNode("Triad (h-h-h-T-p-p)"),
+                createNode("Tapped Slide Extension (h-h-h-T-sl-sl-p-p)"),
+                createNode("Multi-String Extension (h-h-h-h-T-p-p-h)")
+            ])
+        ]),
     ]);
 
     return improvisationTree;
